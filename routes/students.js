7ddddd0fd3a6af306ae9669a27fbb19d20baff25
illagolla <108179002+illagolla@users.noreply.github.com>
2023-02-01 21:38:05 +0530

    first commit

---
+++ routes/students.js
@@ -0,0 +1,88 @@
+const router = require("express").Router();
+const { ConnectionStates } = require("mongoose");
+// const { aggregate } = require("../models/Student");
+let Student = require("../models/Student");
+
+router.route("/add").post((req, res) => {
+  const name = req.body.name;
+  const age = Number(req.body.age);
+  const gender = req.body.gender;
+
+  const newStudent = new Student({
+    name,
+    age,
+    gender,
+  });
+
+  newStudent
+    .save()
+    .then(() => {
+      res.json("Student Added");
+    })
+    .catch(() => {
+      console.log(err);
+    });
+});
+
+router.route("/").get((req, res) => {
+  Student.find()
+    .then((students) => {
+      res.json(students);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+router.route("/update/:id").put(async (req, res) => {
+  let userId = req.params.id;
+  const { name, age, gender } = req.body;
+
+  const updateStudent = {
+    name,
+    age,
+    gender,
+  };
+
+  const update = await Student.findByIdAndUpdate(userId, updateStudent)
+    .then(() => {
+      res.status(200).send({ status: "User updated" });
+    })
+    .catch((err) => {
+      console.log(err);
+      res
+        .status(500)
+        .send({ status: "Error with updating data", error: err.message });
+    });
+});
+
+router.route("/delete/id:").delete(async (req, res) => {
+  let userId = req.params.id;
+
+  const deleteStudent = await Student.findByIdAndDelete(userId)
+    .then(() => {
+      res.status(200).send({ status: "User deleted" });
+    })
+    .catch((err) => {
+      console.log(err);
+      res
+        .status(500)
+        .send({ status: "Error with delet user", error: err.message });
+    });
+});
+
+router.route("/get/id:").get(async (req, res) => {
+  let userId = req.params.id;
+  const user = await Student.findById(userId)
+    .then(() => {
+      res.status(200).send({ status: "User fetched", user: user });
+    })
+    .catch((err) => {
+      console.log(err.message);
+      res
+        .status(500)
+        .send({ status: "Error with get user", error: err.message });
+    });
+});
+
+module.exports = router;
